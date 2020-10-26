dbc3cae6a287b62b837266607158f10671d686e2
lazysomedayz <59799667+lazysomedayz@users.noreply.github.com>
2020-10-27 00:11:33 +0700

    Add files via upload

---
+++ slave/slave/Field.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace slave
+{
+    class Field
+    {
+        public List<Player> players;
+        private string lastcard;
+        private readonly Deck deck;
+
+
+        public Field(Deck deck)
+        {
+            players = new List<Player>();
+            this.deck = deck;
+        }
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+
+        public string GetLatestCard()
+        {
+            return lastcard;
+        }
+
+        public void Play()
+        {
+        start:
+            Intro();
+            CardForPlayers();
+            //CheckStatus();
+            Gameplay();
+            Console.WriteLine("Play Again? (y/n): ");
+            if (Console.ReadLine().ToLower() == "y")
+            {
+                goto start;
+            }
+        }
+
+        private void CardForPlayers()
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                players.ForEach(player => player.AddCard(deck.Deal()));
+            }
+            players.ForEach(player => player.Cardlist.Sort());
+
+        }
+        public void Intro()
+        {
+            Console.WriteLine("============= WELCOME TO SLAVE GAME =============\n");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Enter Player {0} Name : ", i + 1);
+                string Name = Console.ReadLine();
+                AddPlayer(new Player(Name));
+            }
+        }
+        public void Gameplay()
+        {
+            Console.Clear();
+            Console.WriteLine("Latest Card : {0} \n\n\n", lastcard);
+            players.ForEach(players => players.ShowTotalCard());
+            Console.WriteLine("");
+            Console.WriteLine(players[1]);
+            Console.WriteLine("");
+        }
+
+        public void CheckStatus()
+        {
+            foreach (var item in players)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ slave/slave/Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace slave
+{
+    class Player
+    {
+        public List<Card> Cardlist { get; private set; }
+        public bool Pass { get; set; }
+        public string Name { get; set; }
+        public int NumberOfCard => Cardlist.Count;
+
+        public bool have3Club;
+        public int position { get; set; }
+
+        public Player(string name = "Unknown")
+        {
+            this.Name = name;
+            Cardlist = new List<Card>();
+            
+        }
+        public bool Have3Club
+        {
+            get
+            {
+                var CardListStr = new ArrayList();
+                foreach (var item in Cardlist)
+                {
+                    CardListStr.Add(item.ToString());
+                }
+                if (CardListStr.Contains("3♣"))
+                {
+                    return have3Club = true;
+                }
+                return have3Club;
+            }
+            set
+            {
+
+            }
+        }
+
+        public bool WantToPass()
+        {
+            return Pass;
+        }
+
+        public void AddCard(Card card)
+        {
+            Cardlist.Add(card);         
+        }
+
+        public void DropCard(Card card)
+        {
+            Console.WriteLine("Which card do you want to drop in field? \n\n");
+            Console.Write("Select the position of card in your hand. : ");
+            int PositionOfCard = Int32.Parse(Console.ReadLine());
+            Cardlist.RemoveAt(PositionOfCard);
+        }
+
+        public override string ToString()
+        {
+            string returntext = string.Format("==============================================================\n\nName : {0}\n", Name);
+            returntext += string.Format("Cards :");
+            Cardlist.ForEach(card => returntext += string.Format(" {0},", card.ToString()));
+            returntext = returntext.TrimEnd(',');
+
+            returntext += string.Format("\nYour position is : {0}", position);
+
+            returntext += string.Format("\nHave 3♣? : {0}", Have3Club);
+
+            returntext += string.Format("\n\n==============================================================");
+            return returntext;
+        }
+
+        public void ShowTotalCard()
+        {
+            int TotalCard = Cardlist.Count;
+            Console.WriteLine("{0}'s Card remaining: {1}",Name, TotalCard);
+        }
+    }
+}
